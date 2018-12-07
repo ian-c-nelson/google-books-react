d4474fb0d00d69c6ac03bf0d9a4515f913f474df
beef <beeflawg@gmail.com>
2018-12-07 13:38:48 -0700

    updated model and and page for book.js

--- models/book.js
+++ models/book.js
@@ -3,7 +3,7 @@ const Schema = mongoose.Schema;
 
 const bookSchema = new Schema({
   title: { type: String, required: true },
-  author: { type: String, required: true },
+  author: { type: String, required: false },
   synopsis: String,
   date: { type: Date, default: Date.now }
 });
